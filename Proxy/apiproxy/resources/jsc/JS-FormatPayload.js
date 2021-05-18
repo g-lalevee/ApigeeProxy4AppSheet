e38f8b4e3c42cd1de67d71ceefdb7df61b980ddf
Christophe Lalevee <clalevee@gmail.com>
2021-05-18 14:49:49 +0200

    Update payload format

--- Proxy/apiproxy/resources/jsc/JS-FormatPayload.js
+++ Proxy/apiproxy/resources/jsc/JS-FormatPayload.js
@@ -1,9 +1,12 @@
-
 var resource = context.getVariable("urirequest.table");
 var tableRecords = JSON.parse(context.getVariable("extension-output"));
+var result;
 
-// Firestore desn't return item in same order, every time => alphabetical sort
+print("tableRecords: ", tableRecords);
+
+// Firestore doesn't return items in same order, every time => alphabetical sort
 if (Array.isArray(tableRecords)) {
+    print("list");
     var arraySorted = [];
     tableRecords.forEach(obj => {
         var ordered = {};
@@ -12,15 +15,16 @@ if (Array.isArray(tableRecords)) {
         });
         arraySorted.push(ordered);
     });
-    tableRecords = arraySorted;
+    // Add collection name, required by Appsheet for table 
+    result = '{"' + resource+'":' + JSON.stringify(arraySorted) + '}';
 } else {
+    print("item");
     var ordered = {};
     Object.keys(tableRecords).sort().forEach(function(key) {
             ordered[key] = tableRecords[key];
         });
+    // Don't add collection name for object 
+    result = JSON.stringify(ordered);
 }
-// Add collection name, required by Appsheet
-var result = '{"' + resource+'":' + JSON.stringify(tableRecords) + '}';
 
 context.setVariable("result", result);
-
